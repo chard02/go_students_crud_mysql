459bbba6420c850495d2295f45c9d1c6af8f936b
chard02 <paulusaritonang002@gmail.com>
2024-03-23 12:01:43 -1200

    Go

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/chard02/go_students_crud_mysql/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterStudentsRoutes(r)
+	http.Handle("/", r)
+	fmt.Print("Starting Server localhost:9010")
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
